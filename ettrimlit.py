e0a41603092ca78b9ade847cbbfc942f31cda75a
Albondigaesparadrapo <124705845+Albondigaesparadrapo@users.noreply.github.com>
2023-02-14 17:59:08 +0100

    Update ettrimlit.py

--- ettrimlit.py
+++ ettrimlit.py
@@ -17,11 +17,11 @@ resultado = st.radio("Signos",signos)
 st.write(num1,resultado,num2,"=")
 
 if resultado=="+":
-	st.write(int(num1+num2))
+	st.write(num1+num2)
 if resultado=="-":
-	st.write(int(num1-num2))
+	st.write(num1-num2)
 if resultado=="*":
-	st.write(int(num1*num2))
+	st.write(int(num1*num2)
 if resultado=="/":
 	if num2 != 0:
 		st.write(num1/num2)
