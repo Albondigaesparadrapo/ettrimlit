cbe0f61fb0466535fb24581614e2e5f873cc4619
Albondigaesparadrapo <124705845+Albondigaesparadrapo@users.noreply.github.com>
2023-02-14 17:23:13 +0100

    Update ettrimlit.py

--- ettrimlit.py
+++ ettrimlit.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 mensaje = "Para colorear, :red[poner el texto entre corchetes] y delante el nombe del color con :"
 st.markdown(mensaje)
+
+st.succes("Acsión exitosa :3")
+st.info("Esto es solo para informar. Qué inútil")
+st.warning("CUIDADO")
+st.error("Yo te avise")
